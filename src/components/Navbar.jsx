4fccc0f38417edb76a156ca515096841f94689c8
AngelMApps <71051606+AngelMApps@users.noreply.github.com>
2022-08-12 12:24:57 -0500

    modificacion del navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,14 @@
 import {Link} from 'react-router-dom';
+import './Navbar.css';
 const NavBar = ()=> {
     return (
-        <nav>
-            <ul>
-                <li>
+        <nav className='navbar'>
+            <h3 className='nav_title'>API R&M</h3>
+            <ul className='nav_routes'>
+                <li className='nav_routes__li'>
                     <Link to='/' >Home</Link>
                 </li>
-                <li>
+                <li className='nav_routes__li'>
                     <Link to='/search' >search</Link>
                 </li>
             </ul>
